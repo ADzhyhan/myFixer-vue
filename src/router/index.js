45461f548bdf9843db8a672340db398ce717b7f2
ADzhyhan <a.dzhyhan@gmail.com>
2021-05-12 19:16:35 +0300

    add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import SignUp from '../views/SignUp/sign-up.vue';
 import SignIn from '../views/SignIn/sign-in.vue';
+import Recovery from '../views/SignIn/recoveryPassword.vue';
+import Reset from '../views/SignIn/resetPassword.vue'; 
+import SuccessReset from '../views/Success/successReset.vue';
+import SuccessSignUp from '../views/Success/successSignUp.vue';
+import CompleteAccount from '../views/SignUp/createPassword.vue'; 
+import Profile from '../views/Profile/profile.vue'; 
+import Search from '../views/Search/search.vue';
+
 import Home from '../views/Home.vue';
 
 const routes = [
@@ -18,11 +26,42 @@ const routes = [
     path: '/sign-in',
     name: 'SignIn',
     component: SignIn,
-    // // route level code-splitting
-    // // this generates a separate chunk (about.[hash].js) for this route
-    // // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/recovery',
+    name: 'Recovery',
+    component: Recovery
+  },
+  {
+    path: '/reset',
+    name: 'Reset',
+    component: Reset
+  },
+  {
+    path: '/success',
+    name: 'Success',
+    component: SuccessReset
+  },
+  {
+    path: '/activate',
+    name: 'Activate account',
+    component: SuccessSignUp
+  },
+  {
+    path: '/complete',
+    name: 'Complete account',
+    component: CompleteAccount
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search
+  },
 ]
 
 const router = createRouter({
