971c92d419206e817f642ca3e380b900ca9675a9
ADzhyhan <a.dzhyhan@gmail.com>
2021-05-12 19:21:01 +0300

    add links on home-page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,13 @@
     <ul class="home">
       <li class="home__list"><router-link to="/sign-up">Signup</router-link></li>
       <li class="home__list"><router-link to="/sign-in">Signin</router-link></li>
+      <li class="home__list"><router-link to="/recovery">Recovery password</router-link></li>
+      <li class="home__list"><router-link to="/reset">Reset password</router-link></li>
+      <li class="home__list"><router-link to="/success">Success reset password</router-link></li>
+      <li class="home__list"><router-link to="/activate">Success Sign Up</router-link></li>
+      <li class="home__list"><router-link to="/complete">Complete account</router-link></li>
+      <li class="home__list"><router-link to="/profile">Profile</router-link></li>
+      <li class="home__list"><router-link to="/search">Search</router-link></li>
     </ul>
   </main>
 </template>
